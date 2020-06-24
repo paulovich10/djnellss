68c619c8896b3e31206bd3458380128f671bd279
Paulovich <45339575+paulovich10@users.noreply.github.com>
2020-06-24 23:13:26 +0200

    deleted

--- enviatres.php
+++ enviatres.php
@@ -21,7 +21,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'adjnellss@gmail.com';                     // SMTP username
-    $mail->Password   = 'beleza47';                               // SMTP password
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
